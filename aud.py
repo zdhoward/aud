7dd6677aaa8517ed127872106b3c899ef00e81f9
0x01 <zach.d.howard@gmail.com>
2019-04-07 13:34:59 -0700

    more implementation
    requires more testing

--- aud.py
+++ aud.py
@@ -10,7 +10,7 @@ from AudLib import setupLogger, createFolder
 import begin
 
 @begin.start(auto_convert=True)
-def main(file: 'File' = "", dir: 'Directory' = ""):
+def main(file: 'File' = "", dir: 'Directory' = "", rename: 'Rename' = [], convert: 'Convert' = [], pad: 'Padding' = [], metadata: 'Metadata Tags' = [], extensions: 'Extensions' = []):
     '''
     aud: Quick tools for an audio studio environment
     '''
@@ -18,4 +18,93 @@ def main(file: 'File' = "", dir: 'Directory' = ""):
     ## Decipher arguments and run the right command
     ## Designed to work well in the interpreter
     logger.debug("Import to start")
-    pass
+
+    ### SINGLE FILE COMMANDS ###
+    if file != "":
+        _file = AudFile(file)
+        ## RENAME MODULES ##
+        if rename[0].lower() == "lower":
+            _file.renameLower()
+        elif rename[0].lower() == "upper":
+            _file.renameUpper()
+        elif rename[0].lower() == "replacespaces":
+            _file.renameReplaceSpaces()
+        elif rename[0].lower() == "prepend":
+            prefix = rename[1]
+            _file.renamePrepend(prefix)
+
+        ## METADTA MODULES ##
+        if metadata != []:
+            _file.metadata(metadata)
+
+        ## FFMPEG MODULES ##
+        ext = ".wav"
+        sr = "44100"
+        bd = "16"
+        dr = "320k"
+        if convert != []:
+            if convert['extension']:
+                ext = convert['extension']
+            if convert['samplerate']:
+                sr = convert['samplerate']
+            if convert['bitdepth']:
+                bd = convert['bitdepth']
+            if convert['bitrate']:
+                dr = convert['bitrate']
+            _file.convertTo(_extension=ext, _target_samplerate=sr, _target_bitdepth=bd, _target_bitrate=br)
+
+        _in = 0
+        _out = 0
+        if pad != []:
+            if pad['in']:
+                _in = pad['in']
+            if pad['out']:
+                _out = pad['out']
+            _file.pad(_in=_in, _out=_out)
+
+    ### DIR COMMANDS ###
+    elif dir != "":
+        _dir = AudDir(dir)
+        # load extensions
+        if extensions != []:
+            _dir.extenstions = extensions
+
+        ## RENAME MODULES ##
+        if rename[0].lower() == "lower":
+            _dir.renameLower()
+        elif rename[0].lower() == "upper":
+            _dir.renameUpper()
+        elif rename[0].lower() == "replacespaces":
+            _dir.renameReplaceSpaces()
+        elif rename[0].lower() == "prepend":
+            prefix = rename[1]
+            _dir.renamePrepend(prefix)
+
+        ## METADTA MODULES ##
+        if metadata != []:
+            _dir.metadata(metadata)
+
+        ## FFMPEG MODULES ##
+        ext = ".wav"
+        sr = "44100"
+        bd = "16"
+        dr = "320k"
+        if convert != []:
+            if convert['extension']:
+                ext = convert['extension']
+            if convert['samplerate']:
+                sr = convert['samplerate']
+            if convert['bitdepth']:
+                bd = convert['bitdepth']
+            if convert['bitrate']:
+                dr = convert['bitrate']
+            _dir.convertTo(_extension=ext, _target_samplerate=sr, _target_bitdepth=bd, _target_bitrate=br)
+
+        _in = 0.0
+        _out = 0.0
+        if pad != []:
+            if pad['in']:
+                _in = pad['in']
+            if pad['out']:
+                _out =pad['out']
+            _dir.pad(_in=_in, _out=_out)
--- test.py
+++ test.py
@@ -1,12 +1,13 @@
-from AudFile import AudFile
-from AudDir import AudDir
-from AudLib import setupLogger
+import aud
 
-logger = setupLogger()
+#logger = setupLogger()
 #logger.debug("This is a test")
 
-dirpath = AudDir("test")
+#dirpath = AudDir("test")
 
 #dirpath.pad(2.0,2.0)
 #dirpath.renameReplaceSpaces()
-dirpath.convertTo(".wav", _target_samplerate=44100)
+#dirpath.convertTo(".wav", _target_samplerate=44100)
+
+dirpath = aud.AudDir("test")
+logger = aud.setupLogger()
