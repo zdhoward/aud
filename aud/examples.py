7b6dce4580d7b8a4c41a95c4968847cac9a73c03
0x01 <zach.d.howard@gmail.com>
2019-06-07 10:13:04 -0700

    examples + readme

--- aud/examples.py
+++ aud/examples.py
@@ -24,7 +24,11 @@ dirpath.pad(_in=2.0, _out=3.5)
 
 dirpath.log("_Processed")
 
+dirpath.move("C://Share/Audio")
+
 dirpath.convertTo(_extension=".wav", _target_samplerate=44100, _target_bitdepth=16)
+
+dirpath.fade(_in = 5.0, _out = 10.5)
 
 filepath = aud.AudDir("test.wav")
 
@@ -35,6 +39,10 @@ filepath.renamePrepend("sword_sfx_")
 filepath.normalize(_type="peak", _target=0)
 
 filepath.convertTo(_extension=".mp3", _target_samplerate=44100, _target_bitrate="320k") #bitrate="0" for lossy compression
+
+filepath.move("/mnt/share")
+
+filepath.fade(_out = 2.2)
 
 logger.debug(filepath)
 # C:\Folder\test.wav
