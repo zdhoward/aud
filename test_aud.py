ce9d18d2be5868c79ac4f987477403099c203de9
0x01 <zach.d.howard@gmail.com>
2019-06-10 10:55:26 -0700

    Cleaned up tests

--- test_aud.py
+++ test_aud.py
@@ -5,11 +5,7 @@ import aud.AudDir
 ## AUDLIB TESTS
 def test_lib_program_exists():
     assert aud.AudLib.program_exists("ffmpeg")
-
-def test_lib_check_dependencies():
     assert aud.AudLib.check_dependencies()
-
-def test_lib_create_folder():
     assert aud.AudLib.createFolder("./test/")
 
 ## AUDFILE TESTS
@@ -18,34 +14,14 @@ def test_file_moveTo():
     assert file.move(file.base + "../AudDir")
     assert file.move(file.base + "../AudFile")
 
-#def test_file_moveBack():
-#    assert file.move(file.base + "../AudFile")
-
-def test_file_renameUpper():
+def test_rename():
     assert file.renameUpper()
-
-def test_file_renameLower():
     assert file.renameLower()
-
-def test_file_renamePrepend():
     assert file.renamePrepend("test ")
-
-def test_file_renameReplaceSpaces():
     assert file.renameReplaceSpaces('_')
-
-def test_file_cleanupAudFile():
     try:
         import os
         os.rename(file.base + "test_test.wav", file.base + "test.wav")
         assert True
     except:
         assert False
-
-## CLEANUP AFTER TESTS
-#def test_cleanup():
-#    import shutil
-#    try:
-#        shutil.rmtree('./test/')
-#        return True
-#    except:
-#        return False
